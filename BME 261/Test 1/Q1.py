e9beb5c6a0af75e2fdbea22f4dc3e9ce6f986338
f73chen <grace.fenglin@gmail.com>
2021-07-09 13:25:41 -0400

    play tone

---
+++ BME 261/Test 1/Q1.py
@@ -0,0 +1,9 @@
+def add(a, b=0):
+    """
+    Sums two numbers a and b.
+
+    :param a: the first number
+    :param b: the second number
+    :return: the sum of numbers a and b
+    """
+    return a+b
