7b0ff207436dfaaab94cde32b9ee36e3f3a0c95b
f73chen <grace.fenglin@gmail.com>
2022-03-20 20:40:01 -0400

    Deleted all but BME 361

--- BME 261/Test 1/Q1.py
+++
@@ -1,9 +0,0 @@
-def add(a, b=0):
-    """
-    Sums two numbers a and b.
-
-    :param a: the first number
-    :param b: the second number
-    :return: the sum of numbers a and b
-    """
-    return a+b
