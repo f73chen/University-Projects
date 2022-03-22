12463cf0ab1138ece6d81f9dc17283b87722c120
Rebecca Ma <r69ma@uwaterloo.ca>
2022-03-21 23:42:13 -0400

    vectorize functions

--- BME 355/Project/GetMuscleActivationFromWaveform.m
+++ BME 355/Project/GetMuscleActivationFromWaveform.m
@@ -8,12 +8,7 @@ max_point = 5; % defined 5 for our use case, for individual mapping this should 
 
 % apply sigmoid function with modifications (translation and stretch
 % factor) so max x value fits the max dataset
-mapped_waveform = zeros(size(waveform));
-muscle_activation = zeros(size(waveform));
-
-for i = 1:length(waveform)
-    mapped_waveform(i) = 6 * waveform(i) / max_point - 3;
-    muscle_activation(i) = 0.175/(1 + exp(-mapped_waveform(i)));
-end
+mapped_waveform = 6 * waveform / max_point - 3;
+muscle_activation = 0.175 * (1 + exp(-mapped_waveform)) .^ -1;
 
 end
