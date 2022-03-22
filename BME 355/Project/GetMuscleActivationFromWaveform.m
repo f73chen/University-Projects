c2f42568c450f7539cf982897945c1727b8509cb
Rebecca Ma <r69ma@uwaterloo.ca>
2022-03-22 10:32:57 -0400

    minor tweaks to scale factor

--- BME 355/Project/GetMuscleActivationFromWaveform.m
+++ BME 355/Project/GetMuscleActivationFromWaveform.m
@@ -4,11 +4,11 @@ function [muscle_activation] = GetMuscleActivationFromWaveform(t, type, total_s,
 waveform = GetWaveform(t, type, total_s, amp, freq, duty);
 
 % find the max point of the dataset (assume min point is 0)
-max_point = 5; % defined 5 for our use case, for individual mapping this should be max(waveform)
+max_point = 10; % defined for our use case, for individual mapping this should be max(waveform)
+scale_factor = 1;
 
-% apply sigmoid function with modifications (translation and stretch
-% factor) so max x value fits the max dataset
+% apply sigmoid function with modifications
 mapped_waveform = 6 * waveform / max_point - 3;
-muscle_activation = 0.175 ./ (1 + exp(-mapped_waveform));
+muscle_activation = scale_factor ./ (1 + exp(-mapped_waveform));
 
 end
