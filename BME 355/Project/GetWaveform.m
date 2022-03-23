e82ef422bf053daefa138f0b9987dbe4f239c2d8
candaceng <candacewyng@gmail.com>
2022-03-23 12:33:38 -0400

    added start stop times

--- BME 355/Project/GetWaveform.m
+++ BME 355/Project/GetWaveform.m
@@ -1,6 +1,8 @@
-function [waveform] = GetWaveform(t, type, total_s, amp, freq, duty)
+function [waveform] = GetWaveform(t, start, stop, type, total_s, amp, freq, duty)
     % Input Parameters
     %   t:        time in seconds during the gait cycle
+    %   start:    start time of swing phase
+    %   stop:     end time of swing phase
     %   type:     type of waveform (sine, square, trap)
     %   total_s:  number of seconds in one gait cycle
     %   amp:      amplitude - all waves
@@ -9,27 +11,26 @@ function [waveform] = GetWaveform(t, type, total_s, amp, freq, duty)
     % Output
     %   waveform: FES wave from t = 0 to 1 (percent of gait cycle)
     
-    swing = total_s*0.6; % start of the swing phase 
-    
     waveform = zeros(1, size(t, 2));
     for i = 1:size(t,2)
-        if t(i) < swing
+        if t(i) < start || t(i) > stop
             waveform(i) = 0;
         else
             if strcmp(type, 'sine') 
-                waveform(i) = amp/2 * sin(freq*t(i)) + amp/2;
+                waveform(i) = amp/2 * sin(freq*(t(i)-start)) + amp/2;
             elseif strcmp(type, 'square') 
-                waveform(i) = amp/2 * square(freq*t(i), duty) + amp/2;
+                waveform(i) = amp/2 * square(freq*(t(i)-start), duty) + amp/2;
             elseif strcmp(type, 'trap') 
-                margin = (total_s - swing)*(duty/100)/2;
-                if t(i) < swing + margin
-                    waveform(i) = (amp/margin)*t(i) - swing*amp/margin;
-                elseif t(i) > total_s - margin
-                    waveform(i) = -(amp/margin)*t(i) + amp/margin*total_s;
+                margin = (stop - start)*(duty/100)/2;
+                if t(i) < start + margin
+                    waveform(i) = (amp/margin)*t(i) - start*amp/margin;
+                elseif t(i) > stop - margin
+                    waveform(i) = -(amp/margin)*t(i) + amp/margin*stop;
                 else
                     waveform(i) = amp;
                 end
             end
         end
     end
+
 end
