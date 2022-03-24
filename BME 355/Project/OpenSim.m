74b4ef36347cdcbf35d31b0fe4cdfa034ceba308
f73chen <grace.fenglin@gmail.com>
2022-03-24 15:57:07 -0400

    added model function to return reference ankle angle

--- BME 355/Project/OpenSim.m
+++ BME 355/Project/OpenSim.m
@@ -4,12 +4,14 @@ classdef OpenSim < handle
         table
         tilt
         height
+        ankle_angle
         t_sample
     end
     methods
         % Constructor
         function model = OpenSim(filename)
             model.table = table2array(readtable(filename));
+            model.ankle_angle = model.table(:, 6);
             model.tilt = model.table(:, 7);
             model.height = model.table(:, 8);
             model.t_sample = 0:1/60:1.2;
@@ -34,6 +36,17 @@ classdef OpenSim < handle
                 height = model.height(end);
             else
                 height = interp1(model.t_sample, model.height, t);
+            end
+        end
+
+        % Return reference ankle angle
+        function height = GetAnkleAngle(model, t)
+            if t < 0
+                height = model.ankle_angle(1);
+            elseif t > 1.2
+                height = model.ankle_angle(end);
+            else
+                height = interp1(model.t_sample, model.ankle_angle, t);
             end
         end
     end
