ddab623f08a5af42513e2c0195d691b789029385
f73chen <grace.fenglin@gmail.com>
2022-03-24 04:35:55 -0400

    update to assignment 2 solution code

--- BME 355/Project/MatlabForce/force_length_parallel.m
+++ BME 355/Project/MatlabForce/force_length_parallel.m
@@ -1,13 +1,11 @@
 function [normalize_PE_force] = force_length_parallel(lm)
-
-% Input Parameters
-% lm: normalized length of muscle (contractile element)
-% Output
-% normalized force produced by parallel elastic element
-
-% WRITE YOUR CODE HERE
-global parallel_force_length_regression 
-normalize_PE_force = feval(parallel_force_length_regression, lm);
-
+    %%%% TASK 1
+    % Input Parameters
+    %   lm: normalized length of muscle (contractile element)
+    % Output
+    %   normalized force produced by parallel elastic element
+    
+    slack_length = 1; 
+    tight = lm > slack_length; 
+    normalize_PE_force = tight .* (3 * (lm - slack_length).^2 ./ max(1e-6, 0.6 + lm - slack_length));
 end
-
--- BME 355/Project/MatlabForce/force_length_tendon.m
+++ BME 355/Project/MatlabForce/force_length_tendon.m
@@ -1,16 +1,11 @@
 function [normalize_tendon_tension] = force_length_tendon(lt)
-
-% lt: normalized length of tendon (series elastic element)
-% Output
-% normalized tension produced by tendon
-
-% WRITE YOUR CODE HERE
-global tendon_force_length_regression
-
-if lt<=1;
-    normalize_tendon_tension = 0;
-else
-    normalize_tendon_tension = feval(tendon_force_length_regression, lt);
-end
-
+    %%%% TASK 1
+    % Input Parameters
+    %   lt: normalized length of tendon (series elastic element)
+    % Output
+    %   normalized tension produced by tendon
+    
+    slack_length = 1; 
+    tight = lt >= slack_length; 
+    normalize_tendon_tension = tight.*(10 * (lt - slack_length) + 240 * (lt - slack_length).^2); 
 end
