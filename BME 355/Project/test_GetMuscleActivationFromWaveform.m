04d0bdba83377abea60ca0253f2db1a9fbbef85a
f73chen <grace.fenglin@gmail.com>
2022-03-25 02:38:50 -0400

    toe height chart

--- BME 355/Project/test_GetMuscleActivationFromWaveform.m
+++ BME 355/Project/test_GetMuscleActivationFromWaveform.m
@@ -10,25 +10,25 @@ plot(t, GetMuscleActivationFromWaveform(t, start, stop, 'sine', 8, 12, 50, 0.32)
 title('Sine');
 xlabel('Time (s)'); 
 ylabel('Activation');
-ylim([-0.1, 1.1]);
+ylim([-0.025, 1]);
 
 subplot(4,1,2)
 plot(t, GetMuscleActivationFromWaveform(t, start, stop, 'square', 8, 20, 50, 0));
 title('Square');
 xlabel('Time (s)'); 
 ylabel('Activation');
-ylim([-0.1, 1.1]);
+ylim([-0.025, 1]);
 
 subplot(4,1,3)
 plot(t, GetMuscleActivationFromWaveform(t, start, stop, 'trap', 8, 20, 50, 0));
 title('Trapezoidal');
 xlabel('Time (s)'); 
 ylabel('Activation');
-ylim([-0.1, 1.1]);
+ylim([-0.025, 1]);
 
 subplot(4,1,4)
 plot(t, GetMuscleActivationFromWaveform(t, start, stop, 'constant', 8, 20, 50, 0));
 title('Constant');
 xlabel('Time (s)'); 
 ylabel('Voltage (V)');
-ylim([-0.1, 1.1]);
+ylim([-0.025, 1]);
