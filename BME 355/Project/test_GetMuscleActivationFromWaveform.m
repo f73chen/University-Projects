3ba1dc6640fa0aeb1e509e3c5b0ca51818ea5e78
f73chen <grace.fenglin@gmail.com>
2022-03-24 22:59:44 -0400

    Cleaned up a bit to make the presentation

--- BME 355/Project/test_GetMuscleActivationFromWaveform.m
+++ BME 355/Project/test_GetMuscleActivationFromWaveform.m
@@ -2,31 +2,33 @@ clear
 clc
 
 t = 0:0.01:1.2;
+start = 0.35;
+stop = 0.95;
 
 subplot(4,1,1)
-plot(t, GetMuscleActivationFromWaveform(t, 'sine', 10, 12, 50, 0.32));
+plot(t, GetMuscleActivationFromWaveform(t, start, stop, 'sine', 8, 12, 50, 0.32));
 title('Sine');
 xlabel('Time (s)'); 
 ylabel('Activation');
-ylim([0, 1]);
+ylim([-0.1, 1.1]);
 
 subplot(4,1,2)
-plot(t, GetMuscleActivationFromWaveform(t, 'square', 10, 20, 50, 0));
+plot(t, GetMuscleActivationFromWaveform(t, start, stop, 'square', 8, 20, 50, 0));
 title('Square');
 xlabel('Time (s)'); 
 ylabel('Activation');
-ylim([0, 1]);
+ylim([-0.1, 1.1]);
 
 subplot(4,1,3)
-plot(t, GetMuscleActivationFromWaveform(t, 'trap', 10, 20, 50, 0));
+plot(t, GetMuscleActivationFromWaveform(t, start, stop, 'trap', 8, 20, 50, 0));
 title('Trapezoidal');
 xlabel('Time (s)'); 
 ylabel('Activation');
-ylim([0, 1]);
+ylim([-0.1, 1.1]);
 
 subplot(4,1,4)
-plot(t, GetMuscleActivationFromWaveform(t, 'constant', 3, 20, 50, 0));
+plot(t, GetMuscleActivationFromWaveform(t, start, stop, 'constant', 8, 20, 50, 0));
 title('Constant');
 xlabel('Time (s)'); 
 ylabel('Voltage (V)');
-ylim([0, 1]);
+ylim([-0.1, 1.1]);
