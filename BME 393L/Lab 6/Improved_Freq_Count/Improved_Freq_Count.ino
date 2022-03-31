5d50d302553a2d86827191730edeac5fd73d0079
f73chen <55363139+f73chen@users.noreply.github.com>
2022-03-31 11:05:09 -0400

    Update Improved_Freq_Count.ino

--- BME 393L/Lab 6/Improved_Freq_Count/Improved_Freq_Count.ino
+++ BME 393L/Lab 6/Improved_Freq_Count/Improved_Freq_Count.ino
@@ -11,8 +11,7 @@ int lastVal = 1;          // Default state is high
 void setup() {
   Serial.begin(9600);
   pinMode(switchPin, INPUT);  // Set the input pin as an input
-  TCCR0B = ( _BV(CS02) | _BV(CS00));/* Writes to Timer0 register to change it's clock rate
-                                        this would sets bit CS02 and CS00 to 1 so timer clock = clkIO/1024 see "_BV Explained.pdf" on LEARN*/
+  TCCR0B = _BV(CS02);/* Writes to Timer0 register to change it's clock rate this would sets bit CS02 and CS00 to 1 so timer clock = clkIO/1024 see "_BV Explained.pdf" on LEARN*/
 }
 
 void loop() {
@@ -21,10 +20,11 @@ void loop() {
   if (val != lastVal) {
     lastVal = val;
     if (val == LOW) { // Falling edge
-      duration = millis() - startTime;
-      period = float(duration) / 1000;
-      scaledPeriod = 
-      frequency = 1 / period;
+//      duration = millis() - startTime;
+//      period = float(duration) / 1000;
+//      scaledPeriod = period * 16;
+//      frequency = 1 / scaledPeriod;
+      frequency = 250 / float(millis() - startTime);
       Serial.print(" Frequency is = ");
       Serial.println(frequency);
       startTime = millis();
