d04dba51728e58db9ea8ce0e957a51ba0e8c83ee
f73chen <grace.fenglin@gmail.com>
2022-10-21 12:45:52 -0400

    fixed first principles transfer function
    TODO: make simulink models match

--- BME 356L/Lab 1/poles_and_zeros.m
+++ BME 356L/Lab 1/poles_and_zeros.m
@@ -9,18 +9,22 @@ Rm = 0.07;
 
 Jd = 0.5 * Md * Rd^2;
 Jh = 0.5 * Mh * Rh^2;
-Jm = Mm * Rm^2;
+Jm = 2 * Mm * Rm^2;     % Two masses
 Je = 1.7 * 10^-7;
 Jt = Jd + Jh + Jm + Je;
 
 Kt = 0.0241;
 Ra = 3.02;
+Rt = Ra +  2*0.225;     % Add MOSFET resistance
 La = 4.16 * 10^-4;
-Ke = 1/396;
+Ke = (1/396) * 60 / (2*pi);   % Convert to V/(rad/s)
 wm = 4680 * 2*pi / 60;
 ia = 0.0586;
 
 b = Kt*ia/wm;
 
-P = Kt / (Jt*La*s^3 + (Jt*Ra + b*La)*s^2 + (b*Ra + Kt*Ke)*s);
+P = Kt / (Jt*La*s^3 + (Jt*Rt + b*La)*s^2 + (b*Rt + Kt*Ke)*s);
 poles = pole(P);
+
+numerator = Kt / (2.493e-07 * 8341.06665046695)
+denominator = -poles(3)
