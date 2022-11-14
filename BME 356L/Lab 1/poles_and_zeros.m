3bf0f4c842eba3448b2ac9daa54119ed9d657580
f73chen <grace.fenglin@gmail.com>
2022-11-13 21:15:17 -0500

    lab 3 report

--- BME 356L/Lab 1/poles_and_zeros.m
+++ BME 356L/Lab 1/poles_and_zeros.m
@@ -1,31 +1,19 @@
 s = tf('s');
 
-Md = 0.152;
-Rd = 0.0762;
-Mh = 0.012;
-Rh = 0.0127;
-Mm = 0.016;
-Rm = 0.07;
-
-Jd = 0.5 * Md * Rd^2;
-Jh = 0.5 * Mh * Rh^2;
-Jm = 2 * Mm * Rm^2;     % Two masses
-Je = 1.7 * 10^-7;
-Jt = Jd + Jh + Jm + Je;
+m = 0.0128;
+Jt = 5.71E-04;
+l = 0.184;
+g = 9.81;
 
 Kt = 0.0241;
 Ra = 3.02;
 Rt = Ra +  2*0.225;     % Add MOSFET resistance
-La = 4.16 * 10^-4;
+La = 0;
 Ke = (1/396) * 60 / (2*pi);   % Convert to V/(rad/s)
 wm = 4680 * 2*pi / 60;
 ia = 0.0586;
 
 b = Kt*ia/wm;
 
-P = Kt / (Jt*La*s^3 + (Jt*Rt + b*La)*s^2 + (b*Rt + Kt*Ke)*s);
+P = Kt / (Jt*Rt*s^2 + (b*Rt + Ke*Kt)*s - m*g*l*Rt);
 P = minreal(P);
-poles = pole(P);
-
-numerator = Kt / (2.493e-07 * 8341.06665046695)
-denominator = -poles(3)
