fb00bbc3ad1ee7948d76c74a4d6b6dc2203b1efa
f73chen <55363139+f73chen@users.noreply.github.com>
2022-11-22 17:43:18 -0500

    subtract init value

--- BME 386/test_mpu6050/test_mpu6050.ino
+++ BME 386/test_mpu6050/test_mpu6050.ino
@@ -3,6 +3,10 @@
 
 MPU6050 mpu(Wire);
 unsigned long timer = 0;
+bool initialized;
+float init_x = 0;
+float init_y = 0;
+float init_z = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -16,11 +20,27 @@ void loop() {
   mpu.update();
   if((millis()-timer)>1000) {                        // print data every 10ms                                                
     Serial.print("P : ");
-    Serial.println(mpu.getAngleX());
+    Serial.println(mpu.getAngleX() - init_x);
     Serial.print("R : ");
-    Serial.println(mpu.getAngleY());
+    Serial.println(mpu.getAngleY() - init_y);
     Serial.print("Y : ");
-    Serial.println(mpu.getAngleZ());
+    Serial.println(mpu.getAngleZ() - init_z);
+    Serial.println();
     timer = millis();  
+
+    if (!initialized) {
+      init_x = mpu.getAngleX();
+      init_y = mpu.getAngleY();
+      init_z = mpu.getAngleZ();
+      initialized = true;
+    }
   }
+
+//  Serial.print("P : ");
+//  Serial.println(mpu.getAngleX());
+//  Serial.print("R : ");
+//  Serial.println(mpu.getAngleY());
+//  Serial.print("Y : ");
+//  Serial.println(mpu.getAngleZ());
+//  delay(10);
 }
