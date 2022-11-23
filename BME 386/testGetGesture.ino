ea03dfd1d5beb41c54568df818a65ba65e561044
Candace Ng <50592521+candaceng@users.noreply.github.com>
2022-11-22 19:49:41 -0500

    getGesture() function

---
+++ BME 386/testGetGesture.ino
@@ -0,0 +1,48 @@
+const int binaryDist[4][4] = {
+  {0, 0, 0, 0},
+  {0, 0, 0, 0},
+  {1, 1, 1, 1},
+  {1, 1, 1, 1},
+};
+const int gridPixels = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  getGesture();
+}
+
+// Returns the gesture based on binaryDist[][] matrix
+void getGesture() {
+  int count = 0;
+  int numRows = floor(gridPixels / 2);
+  int numPixels = gridPixels * numRows;
+  float ratio = 0.0;
+
+  //Serial.print(numRows);
+  //Serial.print(numPixels);
+
+  for (int i = 0; i < numRows; i++) {
+    for (int j = 0; j < gridPixels; j++) {
+      if (binaryDist[i][j] == 1) {
+        count++;
+      }
+    }
+  }
+
+  ratio = float(count)/float(numPixels);
+  
+  if (ratio > 0.9) {
+    Serial.println("PAPER");
+    delay(1023);
+  } else if (ratio <= 0.1) {
+    Serial.println("ROCK");
+    delay(1023);
+  } else {
+    Serial.println("SCISSORS");
+    delay(1023);
+  }
+}
