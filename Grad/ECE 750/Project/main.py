0de902b96e59fc07502ded8a717d8a196be9a16d
Grace Chen <f73chen@uwaterloo.ca>
2024-11-14 15:41:55 -0500

    Added predict() for features -> discrete action index

--- Grad/ECE 750/Project/main.py
+++ Grad/ECE 750/Project/main.py
@@ -1,7 +1,30 @@
+import gymnasium as gym
+import highway_env
+from highway_env.envs.intersection_env import IntersectionEnv
+import ale_py
+
 from option_critic import OptionCriticFeatures
 
-oc = OptionCriticFeatures(in_features=1,
-                          num_actions=1,
-                          num_options=1)
-oc.save("models/oc_1.pt")
-oc.load("models/oc_1.pt")
+# intersection_config = IntersectionEnv.default_config()
+# intersection_config["observation"]["flatten"] = True
+
+# Feature input, Discrete action space
+# env = gym.make("CartPole-v1", render_mode='rgb_array')      # (4,) --> 2
+# env = gym.make("LunarLander-v3", render_mode='rgb_array')   # (8,) --> 4
+env = gym.make("highway-fast-v0", render_mode='rgb_array')  # (5, 5) --> 5
+# env = gym.make("intersection-v0", render_mode='rgb_array')  # (15, 7) --> 3
+
+# Feature input, Continuous action space
+# env = gym.make("intersection-v1", render_mode='rgb_array')  # (5, 8) --> -1, 1, (2,)
+# env = gym.make("racetrack-v0", render_mode='rgb_array')     # (2, 12, 12)
+
+# Image input, Discrete action space
+# env = gym.make("ALE/Pong-v5", render_mode='rgb_array')      # (210, 160, 3)
+
+obs, info = env.reset()
+
+oc = OptionCriticFeatures(env=env,
+                          num_options=4)
+
+action = oc.predict(obs, deterministic=True)
+print(action)
--- Grad/ECE 750/Project/option_critic.py
+++ Grad/ECE 750/Project/option_critic.py
@@ -1,28 +1,45 @@
 import torch
 import torch.nn as nn
+from torch.distributions import Categorical
 import numpy as np
 from copy import deepcopy
 
 from replay_buffer import ReplayBuffer
 
+# Flattens observations
+# Outputs a discrete action index
 class OptionCriticFeatures(nn.Module):
     # TODO: Add tensorboard_log
     def __init__(self,
-                 in_features,
-                 num_actions,
+                 env,
                  num_options,
-                 device="cpu") -> None:
+                 device="cpu",
+                 temperature=1.0,
+                 epsilon_start=1.0,
+                 epsilon_min=0.1,
+                 epsilon_decay=int(1e6)) -> None:
         super(OptionCriticFeatures, self).__init__()
         
-        self.in_features = in_features
-        self.num_actions = num_actions
+        obs_shape = env.observation_space.shape
+        flattened_obs = np.prod(obs_shape)
+        self.in_features = flattened_obs
+        
+        self.num_actions = env.action_space.n
         self.num_options = num_options
         
         self.device = device
         
+        self.temperature = temperature
+        self.epsilon_start = epsilon_start
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+        
+        # TODO: Make epsilon a class property
+        self.epsilon = 1.0
+        
         # Shared network
         self.features = nn.Sequential(
-            nn.Linear(in_features, 32),
+            nn.Linear(self.in_features, 32),
             nn.ReLU(),
             nn.Linear(32, 64),
             nn.ReLU()
@@ -35,8 +52,9 @@ class OptionCriticFeatures(nn.Module):
         self.terminations = nn.Linear(64, num_options)
         
         # pi_w head weights and biases
-        self.options_W = nn.Parameter(torch.zeros(num_options, 64, num_actions, device=device))
-        self.options_b = nn.Parameter(torch.zeros(num_options, num_actions, device=device))
+        self.options_W = nn.Parameter(torch.empty(num_options, 64, self.num_actions, device=device))
+        torch.nn.init.xavier_uniform_(self.options_W)   # Make weights non-zero
+        self.options_b = nn.Parameter(torch.zeros(num_options, self.num_actions, device=device))
         
         self.to(device)
         
@@ -47,10 +65,49 @@ class OptionCriticFeatures(nn.Module):
     def learn(self, total_timesteps) -> None:
         pass
     
-    # TODO
-    def predict(self, obs, deterministic=True):
-        # Return action, _states
-        pass
+    # Helper: Return the option index with the highest Q_Omega value
+    def greedy_option(self, state):
+        # Calculate Q_Omega for each option
+        # print(state)
+        Q_Omegas = self.Q(state)
+        # print(Q_Omegas)
+        return Q_Omegas.argmax(dim=-1).item()
+    
+    # Helper: Selects an action based on the state and option
+    def get_action(self, state, option):
+        # logits = state * weights + biases (unnormalized action scores for that option)
+        logits = state.data @ self.options_W[option] + self.options_b[option]
+        
+        # Convert logits to softmax probabilities (sums to 1)
+        action_dist = (logits / self.temperature).softmax(dim=-1)
+        
+        # Probability wrapper (adds functionality)
+        action_dist = Categorical(action_dist)
+        
+        # Chooses an action based on the probabilites
+        action = action_dist.sample()
+        logp = action_dist.log_prob(action)
+        entropy = action_dist.entropy()
+        
+        return action.item(), logp, entropy
+    
+    # Selects an action based on the observations
+    def predict(self, obs, deterministic=True) -> int:
+        # Flatten the observations to 1D and add a batch dimension
+        obs_tensor = torch.tensor(obs, dtype=torch.float32).reshape(1, -1).to(self.device)
+        
+        # Get the output of the shared network
+        state = self.features(obs_tensor)
+        
+        # Choose an option
+        if deterministic or np.random.random() > self.epsilon:
+            option = self.greedy_option(state)
+        else:
+            option = np.random.randint(0, self.num_options)
+        
+        # Choose an action based on the option
+        action, logp, entropy = self.get_action(state, option)
+        return action
     
     # Saves the model
     def save(self, filepath) -> None:
